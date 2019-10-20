22a1cf7a632d02997dbd596e7c7aa72b7b0786cb
Divyanshu <37440228+dextroxd@users.noreply.github.com>
2019-10-20 16:19:10 +0530

    Updated ascii file
    Updated some string for ascii character

--- ascii value.cpp
+++ ascii value.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
  char c;
- cout << "Enter a character: ";
+ cout << "Enter a character for making ascii character: ";
  cin >> c;
  cout << "ASCII Value of " << c << " is " << int(c);
  return 0;
