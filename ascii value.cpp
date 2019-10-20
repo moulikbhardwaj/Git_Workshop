6ea35e23b35363ec1720f1dc08055a83a781e6ba
Moulik Bhardwaj <43489811+moulikbhardwaj@users.noreply.github.com>
2019-10-20 16:25:03 +0530

    updated
    updated the code

--- ascii value.cpp
+++ ascii value.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
  char c;
- cout << "Enter a character: ";
+ cout << "Enter a character for the code: ";
  cin >> c;
  cout << "ASCII Value of " << c << " is " << int(c);
  return 0;
